fb5762225e5c2fa794cd89c037ed2c7a5a82182b
georgiantheatre444 <49041615+georgiantheatre444@users.noreply.github.com>
2019-03-28 19:09:15 +0000

    initial commit
    initial push

---
+++ precache-manifest.862924320a27378664d25720ee7544bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2c38e8db5719aa7ce282",
+    "url": "/js/chunk-vendors.00879144.js"
+  },
+  {
+    "revision": "020f586c0c0eb41acb76",
+    "url": "/js/app.1a0650a6.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/js/about.ad763791.js"
+  },
+  {
+    "revision": "99d80aaa9ea243b5b95532b4839e0db8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "020f586c0c0eb41acb76",
+    "url": "/css/app.dde1df84.css"
+  }
+];
